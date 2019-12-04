f085205c4a0ab97ceaacfb07d96272d3c40ce1d8
drew <tindrewmar@gmail.com>
2019-12-04 04:47:04 -0600

    Add files via upload

---
+++ scoreCalculation/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+        //Create a method called calcFeetAndInchesToCentimeters
+       calcFeetAndInchesToCentimeters(9,19);
+
+
+    }
+
+    public static int calcFeetAndInchesToCentimeters(int feet, int inches) {
+        if((feet >= 0) && (inches >=0 && inches <= 12)) {
+            double centimeters
+        } else {
+            return -1;
+        }
+    }
+
+}
