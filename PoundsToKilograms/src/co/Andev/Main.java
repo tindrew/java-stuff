71512e4b6233538e83cd81ee35492c3263139d55
tindrew <tindrewmar@gmail.com>
2019-12-04 15:05:57 -0600

    More java practice examples

---
+++ PoundsToKilograms/src/co/Andev/Main.java
@@ -0,0 +1,10 @@
+package co.Andev;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double pounds = 300d;
+        double poundsToKilograms = pounds * 0.45359237d;
+        System.out.println("your weight in kilograms is " + poundsToKilograms);
+    }
+}
