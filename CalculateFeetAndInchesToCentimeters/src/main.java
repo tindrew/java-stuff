d244b35b5d45ff1f600843b783870c227ec2fb6a
tindrew <tindrewmar@gmail.com>
2019-12-05 02:30:59 -0600

    made some changes to calculateFeetAndInches

--- CalculateFeetAndInchesToCentimeters/src/main.java
+++ CalculateFeetAndInchesToCentimeters/src/main.java
@@ -2,7 +2,6 @@ public class main {
 
     public static void main(String[] args) {
 
-<<<<<<< HEAD
         double CentimeterAmount = calcFeetAndInchesToCentimeters(12, 12);
         System.out.println("The amount of Centimeters is " + CentimeterAmount);
         calcFeetAndInchesToCentimeters(100);
@@ -10,8 +9,8 @@ public class main {
     }
 
 
-    public static double calcFeetAndInchesToCentimeters(int feet, int inches) {
-        int feetToInches =  (feet * 12) + inches;
+    public static double calcFeetAndInchesToCentimeters(double feet, double inches) {
+        double feetToInches =  (feet * 12) + inches;
         double feetToCentimeters = feetToInches * 2.54;
         if(feet >= 0 && inches >=0 && inches <= 12) {
             System.out.println(feetToCentimeters);
@@ -22,38 +21,19 @@ public class main {
         }
     }
 
-    public static double calcFeetAndInchesToCentimeters(int inches) {
+    public static double calcFeetAndInchesToCentimeters(double inches) {
 
-        double feetToCentimeters = inches * 2.54;
-        if(inches >=0) {
-            System.out.println(feetToCentimeters);
-            return feetToCentimeters;
-        } else  {
-            System.out.println("invalid number");
+        if (inches < 0) {
             return -1;
         }
+
+        double feet = (int) inches / 12;
+        double remainingInches = inches % 12;
+        System.out.println(inches + " inches is equal to " + feet + " feet and " + remainingInches + " inches.");
+        return calcFeetAndInchesToCentimeters(feet, remainingInches);
     }
 
 
 
-=======
-        calcFeetAndInchesToCentimeters(12, 12);
-
-
-    }
-
-
-    public static double calcFeetAndInchesToCentimeters(int feet, int inches) {
-        double centimeters = inches / 2.54;
-        double feetToInches = (double) feet / 12;
-        if (feet < 0 || inches < 0 || inches > 12) {
-            return -1;
-        } else if (feet >= 0) {
-            return feetToInches;
-        }
-        return feetToInches;
-    }
-+
->>>>>>> e1a7654ab52b28a4a15d530ec1b35ca0329e2d70
 }
 
