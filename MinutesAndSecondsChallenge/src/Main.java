d07ab013d40d0b1a0c863de6b57a9e0bd8247912
tindrew <tindrewmar@gmail.com>
2019-12-05 19:12:30 -0600

    Added MinutesAndSecondsChallenge for the first time

---
+++ MinutesAndSecondsChallenge/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+
+    public static void main(String[] args) {
+        MinutesAndSeconds.getDurationString(-35,25);
+    }
+}
---
+++ MinutesAndSecondsChallenge/src/MinutesAndSeconds.java
@@ -0,0 +1,13 @@
+public class MinutesAndSeconds {
+
+    public static int getDurationString (int minutes, int seconds) {
+        int hours = 
+
+        if (minutes < 0 || seconds  < 0 || seconds > 59 ) {
+            System.out.println("Invalid value");
+            return -1;
+
+        }
+        return 1;
+    }
+}
