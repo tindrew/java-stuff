8efa88ba8da6f65038fd2d9d8b0bc284971d19b2
tindrew <tindrewmar@gmail.com>
2019-12-07 05:36:42 -0600

    Added MinutesAndSecondsChallenge update

--- MinutesAndSecondsChallenge/src/Main.java
+++ MinutesAndSecondsChallenge/src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-        MinutesAndSeconds.getDurationString(-35,25);
+        System.out.println(MinutesAndSeconds.getDurationString(250,25));
+        System.out.println(MinutesAndSeconds.getDurationString(500));
+        System.out.println(MinutesAndSeconds.getDurationString(3945));
     }
 }
--- MinutesAndSecondsChallenge/src/MinutesAndSeconds.java
+++ MinutesAndSecondsChallenge/src/MinutesAndSeconds.java
@@ -1,13 +1,43 @@
+import org.w3c.dom.ls.LSOutput;
+
 public class MinutesAndSeconds {
 
-    public static int getDurationString (int minutes, int seconds) {
-        int hours = 
+    public static String getDurationString (int minutes, int seconds) {
+
+        int hours = minutes / 60;
+        long totalMinutes = minutes % 60;
 
         if (minutes < 0 || seconds  < 0 || seconds > 59 ) {
-            System.out.println("Invalid value");
-            return -1;
+            return "invalid Value";
+        } else {
+            String hoursString = hours + "h";
+            if(hours < 10) {
+                hoursString = "0" + hoursString;
+            }
 
+            String minutesString = hours + "h";
+            if(hours < 10) {
+                minutesString = "0" + hoursString;
+            }
+            return hours + "H " + totalMinutes + "M " + seconds + "S";
         }
-        return 1;
+
     }
+
+
+    public static String getDurationString ( int seconds) {
+
+        int mins = seconds /60;
+        int remainingSeconds = seconds % 60;
+
+        if (seconds < 0) {
+            System.out.println("invalidValue");
+            return "invalid Value";
+        } else {
+            return getDurationString(mins, remainingSeconds);
+        }
+    }
+
+
 }
+
